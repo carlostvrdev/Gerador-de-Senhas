a696d3959adf5c4291f6eeb3c15ba79df73fc20b
Carlos Eduardo Tavares <carlostvr.01@gmail.com>
2026-06-26 13:34:52 -0300

    Add files via upload

---
+++ Gerador de Senha/script.js
@@ -0,0 +1,42 @@
+let sliderElement = document.querySelector("#slider");
+let burronElement = document.querySelector("#button");
+
+let sizePassword = document.querySelector("#valor");
+let password = document.querySelector("#password");
+
+let containerPassword = document.querySelector("#container-password");
+
+let charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%&*"
+let novaSenha = "";
+
+sizePassword.innerHTML = sliderElement.value;
+
+slider.oninput = function(){
+    sizePassword.innerHTML = this.value;
+}
+
+function generatePassword(){
+
+    let pass = "";
+
+    for(let i = 0, n = charset.length; i < sliderElement.value; ++i){
+        pass += charset.charAt(Math.floor(Math.random() * n));
+    }
+    
+    containerPassword.classList.remove("hide");
+    password.innerHTML = pass;
+    novaSenha = pass;
+
+}
+
+function copyPassword() {
+    if (novaSenha !== "") {
+        navigator.clipboard.writeText(novaSenha)
+            .then(() => {
+                alert("Senha copiada com sucesso!");
+            })
+            .catch(() => {
+                alert("Não foi possível copiar a senha.");
+            });
+    }
+}
